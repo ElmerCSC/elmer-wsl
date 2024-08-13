1904b28469a57bb25ba117c7d9af7a2d2ae409d5
Matias Zwinger <matias.zwinger@csc.fi>
2024-08-13 13:50:21 +0300

    fix: expose mpirun to the user

--- modules/elmer.nix
+++ modules/elmer.nix
@@ -1,10 +1,10 @@
-{ lib, config, inputs, ... }:
+{ lib, config, inputs, pkgs, ... }:
 let
   inherit (lib) mkEnableOption mkIf;
 in
 {
   options.wsl.elmer.enable = mkEnableOption "ElmerFEM";
   config = mkIf config.wsl.elmer.enable {
-    environment.systemPackages = [ inputs.elmer.packages.x86_64-linux.gui ];
+    environment.systemPackages = [ inputs.elmer.packages.x86_64-linux.gui pkgs.mpi ];
   };
 }
